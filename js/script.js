a99d32f164872a203948601b187094adfcb29570
Denis <fordz2012@gmail.com>
2020-06-18 18:14:46 +0300

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let headButton = document.querySelector('.head-form-button');
+let form = document.querySelector('.checking-date-form');
+let formButton = document.querySelector('.button-form');
+
+formButton.onclick = function (evt) {
+  evt.preventDefault();
+  form.classList.add('close-form');
+}
+
+headButton.onclick = function (evt) {
+	evt.preventDefault();
+	form.classList.remove('close-form');
+}
