f7975ae8e616a7be5f89209404c05e9ae2d89a1a
Denis <fordz2012@gmail.com>
2020-06-19 17:54:25 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var headButton = document.querySelector('.head-form-button');
 var form = document.querySelector('.checking-date-form');
 var formButton = document.querySelector('.button-form');
-
+var arrive = document.querySelector('[name=arrive-date]')
 
 headButton.addEventListener("click", function (evt) {
 	
@@ -14,10 +14,11 @@ headButton.addEventListener("click", function (evt) {
 		form.classList.toggle('close-form');
 		form.classList.toggle('open-form');
 	}
+	arrive.focus();
 }); 
 
 
-formButton.addEventListener("click", function (evt) {
+form.addEventListener("submit", function (evt) {
   evt.preventDefault();
   if(form.classList.contains('open-form')) {
 		form.classList.toggle('open-form');
