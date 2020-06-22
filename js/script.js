23d07c2f8a420bf548f0cd2d13c3a1a55102dd48
Denis <fordz2012@gmail.com>
2020-06-22 14:27:00 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,13 @@ var headButton = document.querySelector('.head-form-button');
 var form = document.querySelector('.checking-date-form');
 var formButton = document.querySelector('.button-form');
 var arrive = document.querySelector('[name=arrive-date]')
+var departure = document.querySelector('[name=departure-date]');
+
 
 headButton.addEventListener("click", function (evt) {
 	
 	evt.preventDefault();
+	form.classList.remove('form-error');
 	if(form.classList.contains('open-form')) {
 		
 		form.classList.toggle('open-form');
@@ -19,16 +22,30 @@ headButton.addEventListener("click", function (evt) {
 
 
 form.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  if(form.classList.contains('open-form')) {
+  if(!arrive.value || !departure.value) {
+     evt.preventDefault();
+      form.classList.remove('form-error');
+       form.offsetWidth = form.offsetWidth;
+     form.classList.add('form-error');
+     
+  }
+
+  else {
+  	evt.preventDefault();
+  	form.classList.remove('form-error');
+  	localStorage.setItem("arrive", arrive.value);
+  	localStorage.setItem("departure", departure.value);
+  	if(form.classList.contains('open-form')) {
 		form.classList.toggle('open-form');
 		form.classList.toggle('close-form')
 
 	} else {
+
 	  form.classList.toggle('close-form');
 	   form.classList.toggle('open-form');		
-
 	}
+  }
   
+ 
 }
 );
