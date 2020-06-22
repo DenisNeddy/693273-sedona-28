17ec46fab8ababb277f5cc29b290b32fa9a7fc12
Denis <fordz2012@gmail.com>
2020-06-22 16:31:56 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,44 +8,27 @@ var departure = document.querySelector('[name=departure-date]');
 headButton.addEventListener("click", function (evt) {
 	
 	evt.preventDefault();
-	form.classList.remove('form-error');
-	if(form.classList.contains('open-form')) {
-		
-		form.classList.toggle('open-form');
-		form.classList.toggle('close-form');
-	} else {
-		form.classList.toggle('close-form');
-		form.classList.toggle('open-form');
-	}
+	form.classList.toggle('open-form');
+	
+	
 	arrive.focus();
+
 }); 
 
 
 form.addEventListener("submit", function (evt) {
   if(!arrive.value || !departure.value) {
      evt.preventDefault();
-      form.classList.remove('form-error');
-       form.offsetWidth = form.offsetWidth;
+     form.classList.remove('form-error');
+     form.offsetWidth = form.offsetWidth;
      form.classList.add('form-error');
-     
+    
   }
 
   else {
   	evt.preventDefault();
-  	form.classList.remove('form-error');
-  	localStorage.setItem("arrive", arrive.value);
-  	localStorage.setItem("departure", departure.value);
-  	if(form.classList.contains('open-form')) {
-		form.classList.toggle('open-form');
-		form.classList.toggle('close-form')
-
-	} else {
-
-	  form.classList.toggle('close-form');
-	   form.classList.toggle('open-form');		
-	}
+  	form.classList.toggle('open-form');
+		
   }
-  
  
-}
-);
+});
