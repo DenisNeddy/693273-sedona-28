0898357ca2f3baf531c354e6a2fd1604cbaa3bc7
Denis <fordz2012@gmail.com>
2020-06-23 20:20:53 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,24 @@
-var headButton = document.querySelector('.head-form-button');
-var form = document.querySelector('.checking-date-form');
-var formButton = document.querySelector('.button-form');
-var arrive = document.querySelector('[name=arrive-date]')
-var departure = document.querySelector('[name=departure-date]');
+var headButton = document.querySelector(".head-form-button");
+var form = document.querySelector(".checking-date-form");
+var formButton = document.querySelector(".button-form");
+var arrive = document.querySelector("[name=arrive-date]");
+var departure = document.querySelector("[name=departure-date]");
 
-form.classList.remove('open-form');
+form.classList.remove("open-form");
 
 headButton.addEventListener("click", function (evt) {
-	
 	evt.preventDefault();
-	form.classList.toggle('open-form');
-	
-	
+	form.classList.toggle("open-form");
 	arrive.focus();
-
 }); 
 
 
 form.addEventListener("submit", function (evt) {
   if(!arrive.value || !departure.value) {
      evt.preventDefault();
-     form.classList.remove('form-error');
+     form.classList.remove("form-error");
      form.offsetWidth = form.offsetWidth;
-     form.classList.add('form-error');
-    
+     form.classList.add("form-error");   
   }   
  
 });
